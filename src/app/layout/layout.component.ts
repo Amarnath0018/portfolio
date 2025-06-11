8fe6ba275ca554db90e428557d3c0e893970ba5e
Amarnath Sekar <amarnathsekar13102000@gamil.com>
2025-06-11 08:39:26 +0530

    Layout - user - admin

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { FooterComponent } from "../shared/footer/footer.component";
 import { RouterModule } from '@angular/router';
+import { LandingPageComponent } from "../landing-page/landing-page.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule],
+  imports: [HeaderComponent, FooterComponent, RouterModule, LandingPageComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
