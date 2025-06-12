5f358abceb17526033306eaa11d1b0aa68293f8e
Amarnath Sekar <amarnathsekar13102000@gamil.com>
2025-06-12 23:26:54 +0530

    Landing page UI completion

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [
+      MatIconModule,
+      MatToolbarModule,
+      MatButtonModule
+    ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
